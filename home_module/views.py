cf9f43c863de744db9a6beb39ac60c5ee51d9c42
Bahman Pournazari <bahmanpn@gmail.com>
2024-07-27 02:44:07 +0330

    seprated navbar,header,slider,header_links,footer_links and added render partial for them in base and index templates

--- home_module/views.py
+++ home_module/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'home_module/index.html')
+
+def navbar_component(request):
+    return render(request,'navbar_component.html')
+
+def header_component(request):
+    return render(request,'header_component.html')
+
+def slider_component(request):
+    return render(request,'home_module/slider_component.html')
