5dd4ffc0fcf14f029e4aeeff71f29ba9b4321f93
Bahman Pournazari <bahmanpn@gmail.com>
2024-09-11 20:35:27 +0330

    user profile order details completed

--- user_profile_module/urls.py
+++ user_profile_module/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('dashboard/',views.UserProfileView.as_view(),name='user-profile'),
-    path('orders/',views.UserCompletedOrders.as_view(),name='orders'),
+    path('orders/<int:order_id>/',views.UserCompletedOrders.as_view(),name='order-list'),
     path('profile-details/',views.UserProfileDetail.as_view(),name='profile-details'),
     path('address/',views.UserAddress.as_view(),name='address'),
 ]
--- user_profile_module/views.py
+++ user_profile_module/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.views import View
 from django.db.models import Count
@@ -20,11 +21,13 @@ class UserProfileView(View):
 class UserCompletedOrders(View):
     template_name='user_profile_module/orders.html'
     
-    def get(self,request):
-        return render(request,self.template_name)
-
-    def post(self,request):
-        pass
+    def get(self,request,order_id):
+        target_basket_details=OrderBasket.objects.prefetch_related('order_detail').filter(is_paid=True,user_id=request.user.id).first()
+        if target_basket_details is None:
+            raise Http404('invalid order detail id!!')
+        return render(request,self.template_name,{
+            'target_basket_details':target_basket_details
+        })
 
 
 class UserProfileDetail(View):
